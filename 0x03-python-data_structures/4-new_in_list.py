64b4e731e2b794ab10b22191331ddd1ebc591dbf
Chipo Hameja <chipohameja@gmail.com>
2024-03-19 18:30:43 +0000

    New in list 1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy_list = my_list.copy()
-    copy_list[idx] = element
+
+    if idx > 0 and idx < len(my_list) - 1:
+        copy_list[idx] = element
 
     return copy_list
